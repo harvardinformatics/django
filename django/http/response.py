77b321d8cbd8df7a3ab68344c337d6595f2b7ec0
Aaron Kitzmiller <aaron.kitzmiller@gmail.com>
2020-03-24 10:07:50 -0400

    force samesite lax

--- django/http/response.py
+++ django/http/response.py
@@ -251,6 +251,8 @@ class HttpResponseBase(six.Iterator):
         if httponly:
             self.cookies[key]['httponly'] = True
 
+        self.cookies[key]['samesite'] = 'lax'
+
     def set_signed_cookie(self, key, value, salt='', **kwargs):
         value = signing.get_cookie_signer(salt=key + salt).sign(value)
         return self.set_cookie(key, value, **kwargs)
